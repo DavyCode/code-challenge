fd73d9087ecdf6089edd27f95cfd1969d02cc99e
Davy'd <pdaswift3009@gmail.com>
2018-01-11 21:37:12 +0100

    object method shorthand

--- app/index.js
+++ app/index.js
@@ -57,3 +57,20 @@ console.log({
 );
 */
 
+
+// Use object method shorthand. 
+// bad
+/*const atom = {
+  value: 1,
+  addValue: function (value) {
+    return atom.value + value;
+  },
+};
+// good
+const atom = {
+  value: 1,
+  addValue(value) {
+    return atom.value + value;
+  },
+};
+*/
